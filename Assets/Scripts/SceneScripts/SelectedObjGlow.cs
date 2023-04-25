8b14a35eac162977a73603ead007df95b6b17c0f
bstowemasters <benjamin.stowe-masters20@bathspa.ac.uk>
2023-04-25 14:57:25 +0100

    modified object glow function

--- Assets/Scripts/SceneScripts/SelectedObjGlow.cs
+++ Assets/Scripts/SceneScripts/SelectedObjGlow.cs
@@ -4,70 +4,101 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 
 //[RequireComponent(typeof(Renderer))]
-public class SelectedObjGlow : MonoBehaviour
+public class SelectedObjGlow : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     public GameObject selectedObject;
-    private Renderer currRend;
-    private Material colour;
 
     private bool isSelected;
+    private bool dragging;
+
+    private Material[] origMaterials;
+    private Material glow;
 
 
     void Start()
     {
-        if(selectedObject.GetComponent<MeshRenderer>() == null)
+        MeshRenderer[] meshRend = GetComponentsInChildren<MeshRenderer>();
+        origMaterials = new Material[meshRend.Length];
+
+        for (int i = 0; i < meshRend.Length; i++)
         {
-            selectedObject.AddComponent<MeshRenderer>();
+            origMaterials[i] = meshRend[i].material;
         }
-
-        currRend = selectedObject.GetComponent<MeshRenderer>();
-
-        colour = currRend.material;
-
     }
 
     private void Update()
     {
-        
+        if (dragging && Input.GetMouseButtonUp(0))
+        {
+            disableGlow();
+        }
     }
 
     private void OnMouseDown()
     {
-        if (!EventSystem.current.IsPointerOverGameObject())
+        // Detect if the game object is clicked
+        if (!dragging && !EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-                if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject == selectedObject)
                 {
-                    if (hit.transform.gameObject == selectedObject)
+                    Debug.Log("Prefab clicked!: ");
+                    if (!isSelected)
                     {
-                        Debug.Log("Prefab clicked!: ");
-                        if (isSelected == false)
-                        {
-                            currRend.material.EnableKeyword("_EMISSION");
-                            colour.color = Color.red;
-                            isSelected = true;
-                        }
-                        else
-                        {
-                            currRend.material.DisableKeyword("_EMISSION");
-                            isSelected = false;
-                        }
+
+                        EnableGlow();
+                    }
+                    else
+                    {
+
+                        disableGlow();
+
                     }
                 }
             }
+        }
+    }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragging = true;
+        disableGlow();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragging = false;
+
+    }
+
+    private void EnableGlow() 
+    {
+        MeshRenderer[] meshRend = GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer meshRenderer in meshRend)
+        {
+            meshRenderer.material = glow;
         }
 
-        // Detect if the game object is clicked
-        
+        isSelected = true;
+    }
+    private void disableGlow()
+    {
+        MeshRenderer[] meshRend = GetComponentsInChildren<MeshRenderer>();
 
+        for (int i = 0; i < meshRend.Length; i++)
+        {
+            meshRend[i].material = origMaterials[i];
+        }
+        isSelected = false;
 
     }
 
